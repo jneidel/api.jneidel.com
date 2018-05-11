c06ebcd69c1f544bfe4bf0423716f051f89aba7a
jneidel <jneidel@outlook.com>
2018-05-12 01:54:33 +0200

    Refactor update-manga, update latest on add-manga

--- bin/update-manga.js
+++ bin/update-manga.js
@@ -1,26 +1,5 @@
 #!/usr/bin/env node
 
-const { updateLastChapter, openJson } = require( "../lib/mangareader/manipulate-data" );
-
-/**
- * Update all manga in latest.json
- * Indended to be frequently run as cron job
- */
-
-async function updateLatestManga() {
-  const latest = openJson( "latest" );
-
-  const mangareader = Object.keys( latest.get( "mangareader" ) );
-  const readmng = Object.keys( latest.get( "readmng" ) );
-  const goodmanga = Object.keys( latest.get( "goodmanga" ) );
-
-  await mangareader.forEach( manga => updateLastChapter( manga, "mangareader" ) );
-  await readmng.forEach( manga => updateLastChapter( manga, "readmng" ) );
-  await goodmanga.forEach( manga => updateLastChapter( manga, "goodmanga" ) );
-
-  console.log( "Finished updating" );
-}
+const updateLatestManga = require( "../lib/mangareader/update-latest" );
 
 updateLatestManga();
-
-module.exports = updateLatestManga;
