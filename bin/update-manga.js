6b56ccfdebd4d0c887079313ffd8e5cd454216c0
jneidel <jneidel@outlook.com>
2018-05-11 23:52:24 +0200

    Add goodmanga to update list

--- bin/update-manga.js
+++ bin/update-manga.js
@@ -12,9 +12,13 @@ async function updateLatestManga() {
 
   const mangareader = Object.keys( latest.get( "mangareader" ) );
   const readmng = Object.keys( latest.get( "readmng" ) );
+  const goodmanga = Object.keys( latest.get( "goodmanga" ) );
 
   await mangareader.forEach( manga => updateLastChapter( manga, "mangareader" ) );
   await readmng.forEach( manga => updateLastChapter( manga, "readmng" ) );
+  await goodmanga.forEach( manga => updateLastChapter( manga, "goodmanga" ) );
+
+  console.log( "Finished updating" );
 }
 
 updateLatestManga();
