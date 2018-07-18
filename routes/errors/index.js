163d72d0963222dee46a57b7908eed8e28dcb7e5
Jonathan Neidel <jneidel@outlook.com>
2018-07-18 03:34:49 +0200

    Add error route writing to logs

--- routes/errors/index.js
+++ routes/errors/index.js
@@ -1,15 +1,21 @@
 const express = require( "express" );
 const fs = require( "mz/fs" );
 const path = require( "path" );
-const IncomingError = require("../../models/errors/Error");
+const IncomingError = require( "../../models/errors/Error" );
+const appendToLog = require("../../lib/errors/appendToLog")
 
 const router = express.Router();
 
-router.post( "/submit", ( req, res)=>{
-  const error = IncomingError(req.body)
+router.post( "/submit", ( req, res ) => {
+  const error = IncomingError( req.body );
 
-  res.status(200).json({"success":"true"})
-})
+  // Filter out those that aren't in data/errors/ids
+  if ( error.app.name !== "Missing app.name" ) {
+    appendToLog( error.app.name, error ) 
+  }
+
+  res.status( 200 ).json( { success: "true" } );
+} );
 
 module.exports = router;
 
